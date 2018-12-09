813311c960f5a5941c4ee3999cb17608757f0ec1
Björn <frangiz@gmail.com>
2018-12-09 19:46:04 +0100

    Using the same solution for both part a and b which resulted in a bit better performance for part a.

--- days/day09.py
+++ days/day09.py
@@ -2,6 +2,21 @@
 from collections import deque, defaultdict
 
 
+def play_marbles(players, last_marble_point):
+    scores = defaultdict(int)
+    marbles = deque()
+    marbles.append(0)
+
+    for marble_val in range(1, (last_marble_point + 1)):
+        if marble_val % 23 == 0:
+            marbles.rotate(-7)
+            scores[marble_val % players - 1] += marble_val + marbles.pop()
+        else:
+            marbles.rotate(2)
+            marbles.append(marble_val)
+    return max(scores.values())
+
+
 def part_a(puzzle_input):
     """
     Calculate the answer for part_a.
@@ -14,24 +29,10 @@ def part_a(puzzle_input):
     """
     instructions = ''.join(puzzle_input).split()
     players = int(instructions[0])
-    last_marble_points = int(instructions[6])
+    last_marble_point = int(instructions[6])
 
-    scores = [0 for _ in range(0, players)]
-    marbles = [0]
-    mc = 0
-
-    for marble_val in range(1, last_marble_points + 1):
-        if marble_val % 23 == 0:
-            score = marble_val
-            to_remove = (mc - 7 - 1) % len(marbles) + 1
-            score += marbles.pop(to_remove)
-            mc = to_remove
-            scores[marble_val % players - 1] += score
-        else:
-            place_pos = (mc + 1) % len(marbles) + 1
-            mc = place_pos
-            marbles.insert(place_pos, marble_val)
-    return str(max(scores))
+    max_score = play_marbles(players, last_marble_point)
+    return str(max_score)
 
 
 def part_b(puzzle_input):
@@ -47,22 +48,8 @@ def part_b(puzzle_input):
     instructions = ''.join(puzzle_input).split()
     players = int(instructions[0])
     last_marble_point = int(instructions[6])
-
-    scores = defaultdict(int)
-    marbles = deque()
-    marbles.append(0)
-
-    for marble_val in range(1, (last_marble_point * 100 + 1)):
-        if marble_val % 23 == 0:
-            score = marble_val
-            marbles.rotate(-7)
-            score += marbles.pop()
-            scores[marble_val % players - 1] += score
-        else:
-            marbles.rotate(2)
-            marbles.append(marble_val)
-
-    return str(max(scores.values()))
+    max_score = play_marbles(players, last_marble_point * 100)
+    return str(max_score)
 
 
 def solve(puzzle_input):
