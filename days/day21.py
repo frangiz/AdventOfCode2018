3fb72dcecd70f02d8efb823c8b1bd89f76515392
Björn <frangiz@gmail.com>
2018-12-22 19:09:36 +0100

    More work on day 21.

--- days/day21.py
+++ days/day21.py
@@ -72,19 +72,47 @@ class Device():
             self.ip = self.registers[self.ip_register] + 1
             lines_executed[self.ip] += 1
             counter += 1
-            if counter % 1_000_000 == 0:
-                print(lines_executed)
+            if self.registers[4] > self.registers[2]:
+                print(self.registers)
+                #print(lines_executed)
+                
             #print('ip: {},  ip_register: {} -> {}'.format(self.ip, self.ip_register, self.registers))
             '''
+                #ip 5
+                seti 123 0 3     <-- registers[3] = 123
+                bani 3 456 3     <-- registers[3] = registers[3] & 456
+                eqri 3 72 3      <-- registers[3] = registerse[3] == 72
+                addr 3 5 5       <-- registers[5] += registers[3]
+                seti 0 0 5       <-- registers[5] = 0
+                5:  seti 0 5 3   <-- registers[3] = 0
+                bori 3 65536 2   <-- registers[2] = registers[3] | 65536
+                seti 832312 1 3  <-- registers[3] = 832312
+                bani 2 255 1     <-- registers[1] = registers[2] & 0xFF
+                addr 3 1 3       <-- registers[3] += registers[1]
+                10: bani 3 16777215 3 <-- registers[3] = registers[3] & 0xFFFFFF
+                muli 3 65899 3      <-- registers[3] = registers[3] * 65899
+                bani 3 16777215 3   <-- registers[3] = registers[3] & 0xFFFFFF
+                gtir 256 2 1        <-- registers[1] = 256 > registers[2]
+                addr 1 5 5          <-- registers[5] += registers[1]
+                15: addi 5 1 5      <-- registers[5] += 1
+                seti 27 7 5         <-- registers[5] = 27
+                17: seti 0 2 1      <-- registers[1] = 0
+
                 Spending lots of time in lines 18-25
-                addi 1 1 4
-                muli 4 256 4
-                gtrr 4 2 4
-                addr 4 5 5 <-- registers[5] += registers[4]
-                addi 5 1 5 <-- registers[5] += 1
-                seti 25 1 5 <-- registers[5] = 25
-                addi 1 1 1 <-- registers[1] += 1
-                seti 17 0 5 <-- registers[5] = 17
+                addi 1 1 4      <-- registers[4] = registers[1] + 1
+                muli 4 256 4    <-- registers[4] *= 256
+                gtrr 4 2 4      <-- registers[4] = registers[4] > registers[2]
+                addr 4 5 5      <-- registers[5] += registers[4]
+                22: addi 5 1 5  <-- registers[5] += 1
+                seti 25 1 5     <-- registers[5] = 25 # <-- break the loop?
+                addi 1 1 1      <-- registers[1] += 1
+                25: seti 17 0 5 <-- registers[5] = 17 # jump back to 18
+
+                setr 1 7 2      <-- registers[2] = registers[1]
+                seti 7 2 5      <-- registers[5] = 7
+                eqrr 3 0 1      <-- registers[1] = registers[3] == registers[0]
+                addr 1 5 5      <-- registers[5] += registers[1]
+                seti 5 5 5      <-- registers[5] = 5
             '''
 
 def part_a(puzzle_input):
@@ -99,6 +127,7 @@ def part_a(puzzle_input):
     """
     d = Device(puzzle_input)
     d.run()
+    print(d.registers)
     return str(0)
 
 
