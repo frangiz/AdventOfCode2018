dffbc0a8467d3c31678d9719923c461b0b12d67f
Björn <frangiz@gmail.com>
2019-01-16 22:37:28 +0100

    Day24 fixed.

--- days/day24.py
+++ days/day24.py
@@ -1,5 +1,90 @@
 """--- Day 24: Immune System Simulator 20XX ---"""
-import helpers
+import re
+from enum import Enum, auto
+
+
+class Team(Enum):
+    IMMUNESYSTEM = auto()
+    INFECTION = auto()
+
+
+class Group():
+    def __init__(self, team, units, hp, weak_to, immune_to, dmg, dmg_type, initiative):
+        self.team = team
+        self.units = units
+        self.hp = hp
+        self.weak_to = weak_to
+        self.immune_to = immune_to
+        self.dmg = dmg
+        self.dmg_type = dmg_type
+        self.initiative = initiative
+        self.target = None
+        self.attacked_by = None
+
+    def __repr__(self):
+        return "{}({!r})".format(self.__class__.__name__, self.__dict__)
+
+    def effective_power(self):
+        return self.units * self.dmg
+
+    def select_target(self, groups):
+        target_stats = (0, 0, 0)
+        for g in groups:
+            if g.team != self.team and g.attacked_by is None:
+                t = (self.calc_damage(g), g.effective_power(), g.initiative)
+                if t[0] > 0 and t > target_stats:
+                    target_stats = t
+                    self.target = g
+        if self.target is not None:
+            self.target.attacked_by = self
+
+    def calc_damage(self, target):
+        if self.dmg_type in target.immune_to:
+            return 0
+        elif self.dmg_type in target.weak_to:
+            return self.effective_power() * 2
+        return self.effective_power()
+
+    def attack(self):
+        if self.target is not None:
+            self.target.units = max(self.target.units - (self.calc_damage(self.target) // self.target.hp), 0)
+
+    def clear_selections(self):
+        self.target = None
+        self.attacked_by = None
+
+
+def parse_groups(puzzle_input):
+    regex = re.compile(r'(\d+) units each with (\d+) hit points \(([a-z;, ]+)\) with an attack that does (\d+) (\w+) damage at initiative (\d+)') # noqa E501
+    regex2 = re.compile(r'(\d+) units each with (\d+) hit points with an attack that does (\d+) (\w+) damage at initiative (\d+)') # noqa E501
+    current_team = None
+    groups = []
+    for line in puzzle_input:
+        if line.startswith('Immune System:'):
+            current_team = Team.IMMUNESYSTEM
+        elif line.startswith('Infection:'):
+            current_team = Team.INFECTION
+        else:
+            s = regex.match(line)
+            if s is not None:
+                units, hp, weak_and_or_immune_to, dmg, dmg_type, initiative = s.groups()
+                weak_to = []
+                immune_to = []
+                special = sorted(weak_and_or_immune_to.split('; '))
+                if special[0].startswith('immune to'):
+                    immune_to = special[0].replace('immune to ', '').split(', ')
+                elif special[0].startswith('weak to'):
+                    weak_to = special[0].replace('weak to ', '').split(', ')
+                if len(special) > 1:
+                    weak_to = special[1].replace('weak to ', '').split(', ')
+                groups.append(Group(current_team, int(units), int(hp), weak_to,
+                                    immune_to, int(dmg), dmg_type, int(initiative)))
+            s = regex2.match(line)
+            if s is not None:
+                units, hp, dmg, dmg_type, initiative = s.groups()
+                groups.append(Group(current_team, int(units), int(hp), [],
+                                    [], int(dmg), dmg_type, int(initiative)))
+    return groups
 
 
 def part_a(puzzle_input):
@@ -12,7 +97,17 @@ def part_a(puzzle_input):
         string: The answer for part_a.
 
     """
-    return str(0)
+    groups = parse_groups(puzzle_input)
+    while len(set([g.team for g in groups])) > 1:
+        groups = sorted(groups, key=lambda g: (g.effective_power(), g.initiative), reverse=True)
+        for g in groups:
+            g.select_target(groups)
+        groups = sorted(groups, key=lambda g: g.initiative, reverse=True)
+        for g in groups:
+            g.attack()
+        [g.clear_selections() for g in groups]
+        groups = [g for g in groups if g.units > 0]
+    return str(sum(g.units for g in groups))
 
 
 def part_b(puzzle_input):
@@ -25,6 +120,25 @@ def part_b(puzzle_input):
         string: The answer for part_b.
 
     """
+    for boost in range(10**5):
+        groups = parse_groups(puzzle_input)
+        for g in groups:
+            if g.team == Team.IMMUNESYSTEM:
+                g.dmg += boost
+        while len(set([g.team for g in groups])) > 1:
+            units = sum(g.units for g in groups)
+            groups = sorted(groups, key=lambda g: (g.effective_power(), g.initiative), reverse=True)
+            for g in groups:
+                g.select_target(groups)
+            groups = sorted(groups, key=lambda g: g.initiative, reverse=True)
+            for g in groups:
+                g.attack()
+            [g.clear_selections() for g in groups]
+            groups = [g for g in groups if g.units > 0]
+            if units == sum(g.units for g in groups):
+                break
+        if all(g.team == Team.IMMUNESYSTEM for g in groups):
+            return str(sum(g.units for g in groups))
     return str(0)
 
 
--- test/test_day24.py
+++ test/test_day24.py
@@ -8,8 +8,15 @@ import helpers
 @ddt
 class MyTestCase(unittest.TestCase): # noqa D101
     @data(
-        [],
-        [])
+        [[
+            'Immune System:',
+            '17 units each with 5390 hit points (weak to radiation, bludgeoning) with an attack that does 4507 fire damage at initiative 2',  # noqa E501
+            '989 units each with 1274 hit points (immune to fire; weak to bludgeoning, slashing) with an attack that does 25 slashing damage at initiative 3',  # noqa E501
+            '',
+            'Infection:',
+            '801 units each with 4706 hit points (weak to radiation) with an attack that does 116 bludgeoning damage at initiative 1',  # noqa E501
+            '4485 units each with 2961 hit points (immune to radiation; weak to fire, cold) with an attack that does 12 slashing damage at initiative 4'],  # noqa E501
+            '5216'])
     @unpack
     def test_example_a(self, test_input, expected): # noqa D102
         result = day24.part_a(test_input)
@@ -17,11 +24,18 @@ class MyTestCase(unittest.TestCase): # noqa D101
 
     def test_answer_part_a(self): # noqa D102
         result = day24.part_a(helpers.get_file_contents('day24.txt'))
-        self.assertEqual(result, '')
+        self.assertEqual(result, '17738')
 
     @data(
-        [],
-        [])
+        [[
+            'Immune System:',
+            '17 units each with 5390 hit points (weak to radiation, bludgeoning) with an attack that does 4507 fire damage at initiative 2',  # noqa E501
+            '989 units each with 1274 hit points (immune to fire; weak to bludgeoning, slashing) with an attack that does 25 slashing damage at initiative 3',  # noqa E501
+            '',
+            'Infection:',
+            '801 units each with 4706 hit points (weak to radiation) with an attack that does 116 bludgeoning damage at initiative 1',  # noqa E501
+            '4485 units each with 2961 hit points (immune to radiation; weak to fire, cold) with an attack that does 12 slashing damage at initiative 4'],  # noqa E501
+            '51'])
     @unpack
     def test_example_b(self, test_input, expected): # noqa D102
         result = day24.part_b(test_input)
@@ -29,4 +43,4 @@ class MyTestCase(unittest.TestCase): # noqa D101
 
     def test_answer_part_b(self): # noqa D102
         result = day24.part_b(helpers.get_file_contents('day24.txt'))
-        self.assertEqual(result, '')
+        self.assertEqual(result, '3057')
